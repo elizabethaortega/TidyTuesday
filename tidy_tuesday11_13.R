cbd04af231f3273ee96492b7839e30f3263f24a8
Elizabeth Ortega <elizabethaortega@gmail.com>
2019-11-13 21:08:34 -0800

    r studio commit.

---
+++ tidy_tuesday11_13.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+library(ggthemes)
+
+cran_code <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-12/loc_cran_packages.csv")
